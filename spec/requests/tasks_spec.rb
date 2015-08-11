c4b8f088565a0cf2fa985e0c52481b6f219782f6
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-08-11 20:40:48 +1000

    Assert page should have task 'go to bed'

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe "Tasks", type: :request do
   describe "GET /tasks" do
     it "display some tasks" do
       visit tasks_path # route name followed by _path
+      page.should have_content 'go to bed'
+
     end
   end
 end
