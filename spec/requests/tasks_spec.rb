167810a63651365246e7e4c95e1e3930df394058
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-08-11 20:39:14 +1000

    Add meaningful comment

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Tasks", type: :request do
   describe "GET /tasks" do
     it "display some tasks" do
-      visit tasks_path
+      visit tasks_path # route name followed by _path
     end
   end
 end
