2c8cb10f42be4ffcc6a60b0190c2979d2debe386
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-08-11 22:09:11 +1000

    Add new test for creating a task

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
   describe "GET /tasks" do
-    it "display some tasks" do
+    it "should display some tasks" do
       @task = Task.create :task => 'go to bed'
 
       visit tasks_path # route name followed by _path
       page.should have_content 'go to bed'
 
+    end
+
+    it "creates a new task" do
+      visit tasks_path
+      fill_in 'Task', :with => 'go to work'
+      save_and_open_page #used launchy gem to let you see what is going on
     end
   end
 end
