14290cebd23a9b3243f2eab42d4fdd1ba51acc00
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-08-11 21:37:32 +1000

    Create task before assertion in test

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "Tasks", type: :request do
   describe "GET /tasks" do
     it "display some tasks" do
+      @task = Task.create :task => 'got to bed'
+      
       visit tasks_path # route name followed by _path
       page.should have_content 'go to bed'
 
