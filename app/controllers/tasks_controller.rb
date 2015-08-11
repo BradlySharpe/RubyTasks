8aa164ba4789c6a12fcdceee22693eff49e31679
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-08-11 22:28:52 +1000

    Add Create task and redirect back

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,10 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all # @ symbol makes it an instance variable available in the views
   end
+
+  def create
+    #render :text => params.inspect # Shows $_GET & $_POST vars
+    Task.create params[:task]
+    redirect_to :back
+  end
 end
